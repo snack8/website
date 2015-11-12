174fb088502e99323eefb4c8df9152eb8e50ef84
Jack Christoforo <jack@christoforo.org>
2015-11-12 14:40:22 -0800

    Initial commit take 2

---
+++ src/js/scripts.js
@@ -0,0 +1,57 @@
+(function ($, window, document, undefined) {
+
+  /* jshint strict: false */
+
+  $(function () {
+
+    if( window.navigator.userAgent.indexOf('MSIE ') < 0){
+      var $background = $('#background');
+      var backgroundVideo = $background.children('video')[0];
+      backgroundVideo.addEventListener('canplaythrough', function(){
+        this.style.opacity = 1;
+        this.play();
+      });
+    }
+    var $greetingTicker = $('.greeting');
+    if($greetingTicker){
+
+      var tickerBegin = function( ticker ){
+        var greetings = ['My name is Jack', 'I am a web developer', 'I have a dog', 'I live in Seattle'];
+        var messageLength = 4;
+
+        tickerLooper(ticker, greetings, messageLength, 0);
+      };
+
+      var tickerLooper = function( $ticker, greetings, messageLength, greetingIndex ){
+        $ticker.html(greetings[greetingIndex]);
+        fadeIn( $ticker );
+        window.setTimeout(function(){
+          fadeOut( $ticker, function(){
+            tickerLooper( $ticker, greetings, messageLength, ++greetingIndex % greetings.length );
+          } );
+        }, messageLength * 1000 );
+      };
+
+      var fadeIn = function( $ticker ){
+        $ticker.removeClass("animated fadeInDown fadeOutDown");
+        $ticker.addClass("animated fadeInDown");
+      };
+
+      var fadeOut = function( $ticker, callback ){
+        $ticker.removeClass("animated fadeInDown fadeOutDown");
+        $ticker.addClass("animated fadeOutDown");
+        window.setTimeout(function(){
+          callback();
+        }, 1000 );
+      };
+
+      window.setTimeout(function(){
+        tickerBegin($greetingTicker);
+      }, 1000);
+    }
+
+  });
+
+
+
+})(jQuery, window, document);
