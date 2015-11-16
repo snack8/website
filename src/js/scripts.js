fbf2219c47295a4e935fe3cfc2e985706f7962d7
Jack Christoforo <jack@christoforo.org>
2015-11-15 18:06:18 -0800

    Why does VIM baffel me so...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,8 +12,10 @@
         this.play();
       });
     }
+
+
     var $greetingTicker = $('.greeting');
-    if($greetingTicker){
+    if( $greetingTicker.size() ){
 
       var tickerBegin = function( ticker ){
         var greetings = ['My name is Jack', 'I am a web developer', 'I have a dog', 'I live in Seattle'];
@@ -33,13 +35,13 @@
       };
 
       var fadeIn = function( $ticker ){
-        $ticker.removeClass("animated fadeInDown fadeOutDown");
-        $ticker.addClass("animated fadeInDown");
+        $ticker.removeClass('animated fadeInDown fadeOutDown');
+        $ticker.addClass('animated fadeInDown');
       };
 
       var fadeOut = function( $ticker, callback ){
-        $ticker.removeClass("animated fadeInDown fadeOutDown");
-        $ticker.addClass("animated fadeOutDown");
+        $ticker.removeClass('animated fadeInDown fadeOutDown');
+        $ticker.addClass('animated fadeOutDown');
         window.setTimeout(function(){
           callback();
         }, 1000 );
@@ -48,6 +50,26 @@
       window.setTimeout(function(){
         tickerBegin($greetingTicker);
       }, 1000);
+    }
+
+
+    $('.tab').each( function(){
+      var $tab = $( this );
+      var $content = buildLightboxContent( $tab );
+      var configuration = getLightboxConfig( $tab );
+
+      $tab.featherlight( $content, configuration );
+    });
+
+    function buildLightboxContent( $thisTab ){
+      var $lightboxContent = $('<div/>');
+      
+      return $lightboxContent;
+    }
+
+    function getLightboxConfig( $thisTab ){
+      var thisContent = {};
+      return thisContent;
     }
 
   });
